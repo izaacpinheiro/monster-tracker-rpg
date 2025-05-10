9b7ecf7110cb360aea86b661e366c2e465f92736
aizac13 <izaacpinheiro.123@gmail.com>
2025-05-10 00:14:10 -0300

    Funções setName setLife e copy

--- src/app/Monster.java
+++ src/app/Monster.java
@@ -15,11 +15,23 @@ public class Monster {
         if (life < 0) life = 0;
     }
 
+    public void setLife(int life){
+        this.life = life;
+    }
+
     public int getLife(){
         return life;
     }
 
+    public void setName(String name){
+        this.name = name;
+    }
+
     public String getName(){
         return name;
+    }
+
+    public Monster copy(){
+        return new Monster(this.name, this.life);
     }
 }
